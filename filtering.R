ddcd948037d021cde507a943764438c6eb7f7e17
Alexey Davydov <alexey.davydov@ymail.com>
2021-05-31 12:55:07 -0700

    almost done

---
+++ filtering.R
@@ -0,0 +1,27 @@
+#rm(list=ls())
+
+data <- read_csv("hfi_cc_2020.csv")
+
+religious_vs_hf <- data %>% 
+  group_by(year) %>% 
+  select(hf_rank, pf_religion_restrictions)
+
+legal_restriction_yrs <- data %>% 
+  group_by(year) %>% 
+  select(ef_legal)
+
+legal_restriction_rgn <- data %>% 
+  group_by(region) %>% 
+  select(ef_legal)
+
+top_money_growth %>% data %>% 
+  arrange(desc(ef_money_growth)) %>% 
+  head(5)
+
+bottom_money_growth %>% data %>% 
+  arrange(ef_money_growth) %>% 
+  head(5)
+
+country_vs_hf <- data %>% 
+  group_by(countries) %>% 
+  select(hf_rank, countries)
