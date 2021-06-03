7a299232607f1266b1bc5c841403e91a2ff733d6
aryamangala <aryamangala@Aryamans-MacBook-Air.local>
2021-06-02 18:12:20 -0700

    Latest changes to server

--- Final_Project_App/server.R
+++ Final_Project_App/server.R
@@ -146,14 +146,9 @@ shinyServer(function(input, output) {
     })
     
     output$legalRestrictionsText <- renderText({
-<<<<<<< HEAD
             paste("The most legal restrictions were in ", summaryLegalRestrictions()[1,1], "with a value of ", summaryLegalRestrictions()[1,2], "and has an human freedom rank of", summaryLegalRestrictions()[1,3])
         }
     )
-=======
-            paste("The most legal restrictions were in ", summaryLegalRestrictions()[1,1], "with a value of ", summaryLegalRestrictions()[1,2])
-    })
->>>>>>> d7afd252f880326cbbdc2f9f926a6d51210a7f97
     
     output$legalRestrictionsSummary <- renderText({
         print("This tab displays a breakdown of how many legal restrictions a given 
@@ -162,7 +157,6 @@ shinyServer(function(input, output) {
               countries in that given region and the number of legal restrictions 
               recorded in coefficient form.")
     })
-<<<<<<< HEAD
     
     output$legal_analysis <- renderText({
       print("The bar plot shows that, counterintuitive to our expectation, countries with greater legal restrictions have a higher human freedom rank.
@@ -194,9 +188,7 @@ shinyServer(function(input, output) {
     output$overview_group <- renderText({
       print("Aryaman Gala and Alexey Davydov")
     })
-      
-    })
-=======
+
 })
->>>>>>> d7afd252f880326cbbdc2f9f926a6d51210a7f97
+
 
